ba4d3db8e859a35bd7e297e16d1a724683809148
ricardo tizatto <jr.ferrugem@hotmail.com>
2015-02-02 09:30:21 -0200

    Commit repToExcel

---
+++ src/timer/Task.java
@@ -0,0 +1,119 @@
+package timer;
+
+import java.awt.Toolkit;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Timer;
+import java.util.TimerTask;
+import jdk.nashorn.tools.Shell;
+
+public class Task {
+
+    private static File movedFile;
+
+    Timer timer;
+    private final Toolkit toolkit;
+    private String configurationFile;
+
+    public Task(int seconds) {
+        toolkit = Toolkit.getDefaultToolkit();
+        timer = new Timer();
+        timer.schedule(new NewTask(), 0, 1 * 1000);
+    }
+
+    class NewTask extends TimerTask {
+
+        Timer counter = null;
+        String entry = "D:\\teste";
+
+        public void run() {
+            if (counter == null) {
+                System.out.println("Reading");
+                File source = new File("X:\\Unicoop\\Relatorios para converter");
+                File destiny = new File("X:\\Unicoop\\Relatorios convertidos");
+
+                try {
+                    copyAll(source, destiny, false);
+                } catch (IOException ex) {
+                }
+
+            } else {
+                System.out.println("Acabou de executar");
+                System.exit(0);
+            }
+        }
+    }
+
+    public static void copyAll(File source, File destiny, boolean modification) throws IOException, UnsupportedOperationException {
+
+        if (!destiny.exists()) {
+            destiny.mkdir();
+        }
+        if (!source.isDirectory()) {
+            throw new UnsupportedOperationException("Origem deve ser um diretório");
+        }
+        if (!destiny.isDirectory()) {
+            throw new UnsupportedOperationException("Destino deve ser um diretório");
+        }
+        File[] files = source.listFiles();
+
+        for (int i = 0; i < files.length; ++i) {
+
+            System.out.println("Copiando arquivo: " + files[i].getName());
+            movedFile = new File(destiny + "\\" + files[i].getName());
+            copy(files[i], movedFile, modification);
+            files[i].delete();
+            runRep2Excel(movedFile);
+        }
+    }
+
+    public static void copy(File source, File destiny, boolean modification) throws IOException {
+
+        if (destiny.exists() && !modification) {
+            return;
+        }
+        source.renameTo(destiny);
+
+    }
+
+    public static String runRep2Excel(File movedFile) throws IOException {
+        boolean success = false;
+        String result;
+
+        Process p;
+        BufferedReader input;
+        StringBuffer cmdOut = new StringBuffer();
+        String lineOut = null;
+        int numberOfOutline = 0;
+
+        try {
+
+            p = Runtime.getRuntime().exec("X:\\Unicoop\\Rep2Excel\\rep2excel-trial\\Rep2excel.exe  -i:" + movedFile.getPath());
+
+            input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            while ((lineOut = input.readLine()) != null) {
+                if (numberOfOutline > 0) {
+                    cmdOut.append("\n");
+                }
+                cmdOut.append(lineOut);
+                numberOfOutline++;
+            }
+
+            result = cmdOut.toString();
+            success = true;
+            input.close();
+
+        } catch (IOException e) {
+            result = String.format("Falha ao executar comando %s. Erro: %s", Shell.COMMANDLINE_ERROR, e.toString());
+        }
+
+        if (!success) {
+            throw new IOException(result);
+        }
+
+        return result;
+    }
+}
---
+++ src/timer/Timer.java
@@ -0,0 +1,12 @@
+package timer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException; 
+
+public class Timer {
+
+    public static void main(String[] args) throws FileNotFoundException, IOException {
+        System.out.println("Começou a ler a Pasta.");
+    }
+
+}
